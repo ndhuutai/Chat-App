40a62f29ebb7cb74660a6896378f372ea080aaf2
ndhuutai <ndhuutai1@gmail.com>
2019-04-24 17:28:57 -0700

    removed scaffolded files

--- ClientApp/src/store/configureStore.js
+++ ClientApp/src/store/configureStore.js
@@ -1,8 +1,6 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
-import * as Counter from './Counter';
-import * as WeatherForecasts from './WeatherForecasts';
 import CommentsReducer from '../reducers/CommentsReducer';
 import ConnectionReducer from '../reducers/ConnectionReducer';
 import UserReducer from '../reducers/UserReducer';
