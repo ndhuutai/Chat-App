603e3358bb47932c9e13c179d87784abcb320fec
ndhuutai <ndhuutai1@gmail.com>
2019-04-26 12:35:46 -0700

    added group chat functionality

--- Models/Hubs/ChatHub.cs
+++ Models/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -8,28 +9,31 @@ namespace Chat_App.Models.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessageToGroup(string message, string userName, string avatarUrl, string group)
         {
-            await Clients.All.SendAsync("ReceiveMessage",  message);
+            await Clients
+                .Group(group)
+                .SendAsync("MessageToGroup", userName,  message, avatarUrl);
             
         }
 
         public override async Task OnConnectedAsync()
         {
-            await Clients.Caller.SendAsync("ServerMessage", Context.ConnectionId, "Welcome to the chat room");
+            await Clients.Caller.SendAsync("ServerMessageOnConnected", Context.ConnectionId);
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            await Clients.Caller.SendAsync("ServerMessage", "A client disconnected from the chat room");
+            await Clients.Caller.SendAsync("ServerMessageOnDisconnected", "A client disconnected from the chat room");
             await base.OnDisconnectedAsync(exception);
         }
 
         public async Task AddToGroup(string groupName, string userName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("ServerToGroup", userName);
+            await Clients.Caller.SendAsync("ServerMessageOnConnectedToGroup", $"Welcome to the group {userName}!");
+            await Clients.OthersInGroup(groupName).SendAsync("ServerToGroup", userName);
         }
     }
 }
