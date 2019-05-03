67a986de6f54128d81eaa9302c205d31795daceb
ndhuutai <ndhuutai1@gmail.com>
2019-05-02 18:19:43 -0700

    moved git files up a folder

---
+++ Chat-App.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System;
+using Chat_App.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var options = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            var context = new RepositoryContext(options);
+
+            var commentManager = new CommentManager(context);
+
+            var comment = new Comment()
+            {
+                AvatarUrl = "myavatarurl.com",
+                CreatedAt = DateTime.Now,
+                Id = 1,
+                Text = "my test comment",
+                UserId = 1
+            };
+
+            commentManager.Add(comment);
+            var returnComment = commentManager.Get(1);
+            Assert.AreEqual(comment, returnComment);
+        }
+    }
+}
